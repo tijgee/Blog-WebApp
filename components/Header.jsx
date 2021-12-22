49ae17df3b793ca903ee12a31764f43a2c411945
Timothy Gee <tijgee@ucsc.edu>
2021-12-22 02:46:31 -0800

    created navbar

---
+++ components/Header.jsx
@@ -0,0 +1,34 @@
+//nav bar
+import React, { useContext} from 'react'
+
+import Link from 'next/link';
+
+const categories = [{name: 'React', slug: 'react' }, { name: 'Web Development', slug: 'web-dev'} ]
+const Header = () => {
+    return (
+        <div className= "container mx-auto px-10 mb-8">
+            <div className ="border-b w-full inline-block border-blue-5-- py-8">
+                <div className="md:float-left blockl">
+                    <Link href="/">
+                        <span className="cursor-pointer font-bold text-4xl text-white">
+                            GraphCMS
+                            </span>
+                    </Link>
+                </div>
+                {/*categories*/}
+                <div className="hidden md:float-left md:contents"> {/*make categories hidden on small devices*/}
+                    {categories.map((category) => (
+                        <Link key={category.slug} href={`/category/${category.slug}`}>
+                            <span className ="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                                {category.name}
+                            </span>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
